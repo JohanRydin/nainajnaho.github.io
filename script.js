325a84f5a82c1c7569b87d2251a34b26f279d52d
David Ohanjanian <87942705+nainajnahO@users.noreply.github.com>
2023-04-05 20:34:22 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-(function(d, script) {
-  script = d.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.src = 'https://termify.io/script/642db8acd1dbc.js';
-  d.getElementsByTagName('head')[0].appendChild(script);
-}(document));
